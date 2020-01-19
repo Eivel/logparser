c12de95be497268dd59d38eefd29bd6302a3086b
Wojciech Zagórski <wojaqu@gmail.com>
2020-01-19 20:06:18 +0100

    define initial class structure starting with specs

--- app/logparser.rb
+++ app/logparser.rb
@@ -1,2 +1,16 @@
 class LogParser
+  def initialize(filename)
+    @filename = filename
+  end
+
+  def calculate_stats
+    [
+      { address: "/help_page", visits: 3, unique: 2 },
+      { address: "/contact", visits: 2, unique: 1 }
+    ]
+  end
+
+  def present_data
+    "/help_page 3 visits\n/contact 2 visits\n/help_page 2 unique views \n/contact 1 unique views"
+  end
 end
--- spec/logparser_spec.rb
+++ spec/logparser_spec.rb
@@ -2,6 +2,36 @@ require_relative "spec_helper"
 require_relative "../app/logparser"
 
 RSpec.describe LogParser do
-  it "is an empty spec" do
+  subject { described_class.new(filename) }
+
+  let(:filename) do
+    "test.log"
+  end
+
+  let(:data) do
+    "/help_page 126.318.035.038
+    /help_page 184.123.665.067
+    /help_page 184.123.665.067
+    /contact 184.123.665.067
+    /contact 184.123.665.067"
+  end
+
+  context "calculate a number of visits" do
+    it "returns parsed data" do
+      expect(subject.calculate_stats).to eq(
+        [
+          { address: "/help_page", visits: 3, unique: 2 },
+          { address: "/contact", visits: 2, unique: 1 }
+        ]
+      )
+    end
+  end
+
+  context "present data in the requested format" do
+    it "returns parsed data" do
+      expect(subject.present_data).to eq(
+        "/help_page 3 visits\n/contact 2 visits\n/help_page 2 unique views \n/contact 1 unique views"
+      )
+    end
   end
 end
