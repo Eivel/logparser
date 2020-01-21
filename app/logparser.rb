9d883d96357b7633402a28a84f34bfcdd8ce8211
Wojciech Zagórski <wojaqu@gmail.com>
2020-01-21 20:00:07 +0100

    remove data processing from logparser class

--- app/logparser.rb
+++ app/logparser.rb
@@ -1,4 +1,4 @@
-# LogParser processes log files to produce general access stats.
+# LogParser processes log files to produce selected metrics output.
 class LogParser
   def initialize(filepath)
     raise IOError, 'File does not exist.' unless File.file?(filepath)
@@ -6,69 +6,24 @@ class LogParser
     @filepath = filepath
   end
 
-  def calculate_stats
-    stats = load_visits_and_ips
-    stats = inject_unique_visits(stats)
-
-    stats
-  end
-
-  def present_data
-    stats = calculate_stats
-
-    "#{visits_output(stats)}\n#{unique_views_output(stats)}"
+  def present_metrics(*metric_classes)
+    metrics = initialize_metrics(metric_classes)
+    metrics = process_file(metrics)
+    metrics.map(&:text_output).join("\n\n")
   end
 
   private
 
-  def visits_output(stats)
-    stats
-      .sort_by { |_, details| details[:visits] }
-      .map { |address, details| "#{address} #{details[:visits]} visits" }
-      .reverse
-      .join("\n")
+  def initialize_metrics(metric_classes)
+    metric_classes.map(&:new)
   end
 
-  def unique_views_output(stats)
-    stats
-      .sort_by { |_, details| details[:unique] }
-      .map { |address, details| "#{address} #{details[:unique]} unique views" }
-      .reverse
-      .join("\n")
-  end
-
-  def load_visits_and_ips
-    stats = {}
+  def process_file(metrics)
     File.foreach(@filepath) do |line|
-      check_line_format(line)
-      address, ip = line.split(' ')
-      stats[address] = update_stats(stats[address], ip)
+      metrics.each do |metric|
+        metric.process_line(line)
+      end
     end
-    stats
-  end
-
-  def update_stats(details, ip)
-    if details
-      details[:visits] += 1
-      details[:ips][ip] = true
-      details
-    else
-      {
-        visits: 1,
-        ips: { ip => true }
-      }
-    end
-  end
-
-  def check_line_format(line)
-    regexp = %r{^\/\S+\s\d+\.\d+\.\d+\.\d+$}
-    raise RegexpError, 'Incorrect line format.' unless regexp.match?(line)
-  end
-
-  def inject_unique_visits(stats)
-    stats.each do |address, _|
-      ips = stats[address].delete(:ips)
-      stats[address][:unique] = ips.length
-    end
+    metrics
   end
 end
